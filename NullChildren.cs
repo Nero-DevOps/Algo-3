7c1a27513e99acc17389f36d6cb1a7afe4ab32ec
Nero-DevOps <81783723+Nero-DevOps@users.noreply.github.com>
2022-01-24 23:09:10 +0000

    NullChilderen
    My source file

---
+++ NullChildren.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace ConsoleApp3
+{
+    public partial class QuadTree<T>
+    {    
+        [Serializable]
+        private sealed class NullChildren<T> : Children<T>
+        {
+            public NullChildren(Node<T> parent)
+                : base(parent, null, null, null, null)
+            {
+
+            }
+
+            public override Node<T> GetContainingChild(Point point)
+            {
+                return null;
+            }
+        }
+    }
+}
